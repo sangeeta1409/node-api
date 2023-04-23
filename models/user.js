945138f75c2d203bbc46a37bb6efa59c911f331e
Sangeeta <rupashree074@gmail.com>
2023-04-23 16:26:29 +0530

    initial commit

--- models/user.js
+++ models/user.js
@@ -1,44 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-class User extends Model {}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
     role: {
       type: DataTypes.STRING,
       allowNull: false,
       defaultValue: 'user',
-      isIn: [['user', 'admin']],
+      isIn: [['user', 'admin']]
     },
     otp: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    otpExpires: {
+    otpExpires:
+    {
       type: DataTypes.DATE,
       allowNull: true,
       defaultValue: null,
     },
-  },
-  {
+  }, {
     sequelize,
     modelName: 'User',
-  }
-);
-
-module.exports = User;
+  });
+  return User;
+};
